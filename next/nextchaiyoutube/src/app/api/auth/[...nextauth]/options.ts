f494782bc171508cf1db4b3d9ccc9f1186389397
MD FIROJ AHMED <firozahmed3052000@gmail.com>
2024-06-25 03:26:18 +0000

    auth error handel =

--- next/nextchaiyoutube/src/app/api/auth/[...nextauth]/options.ts
+++ next/nextchaiyoutube/src/app/api/auth/[...nextauth]/options.ts
@@ -3,47 +3,43 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import dbConnect from "@/lib/config/db";
 import UserModel from "@/lib/model/User";
 
-export const authOptions:NextAuthOptions{
-    providers:[
+export const authOptions: NextAuthOptions = {
+    providers: [
         CredentialsProvider({
-            id:"Credentials",
-            name:"Credentials",
-            credentials:{
-                email:{label:"Username",type:"email",placeholder:"Enter your email"},
-                password:{label:"Password",type:"password",placeholder:"Enter your password"}
+            id: "credentials",
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "email", placeholder: "Enter your email" },
+                password: { label: "Password", type: "password", placeholder: "Enter your password" },
             },
-            async authorize(credentials: any): Promise<any>{
+            async authorize(credentials: any): Promise<any> {
                 await dbConnect();
                 try {
-                    const user = await UserModel.findOne({ 
-                        $or:[
-                            {userName:credentials.username},{email:credentials.email}
-                        ]
-                        });
-                    if (!user){
+                    const user = await UserModel.findOne({
+                        $or: [{ userName: credentials.username }, { email: credentials.email }],
+                    });
+                    if (!user) {
                         throw new Error("No User Found");
                     }
-                    if (!user.isVerified){
-                        throw new Error("Plz Verify Your Email");
+                    if (!user.isVerified) {
+                        throw new Error("Please Verify Your Email");
                     }
-                    if (user.password===credentials.password){
-                        return user;
-                    }else{
+                    if (credentials.password === user.password) {
                         throw new Error("Invalid Password");
                     }
-                } catch (error:any) {
+                    return user;
+                } catch (error: any) {
                     console.warn(error.message);
+                    return null; // Returning null indicates failed authentication
                 }
-                
-                
-            }
-        })
-    ]
-    /*pages:{
-        signIn: 'sign-in'
+            },
+        }),
+    ],
+    pages: {
+        signIn: "/sign-in",
     },
-    session:{
-        strategy:'jwt',
+    session: {
+        strategy: "jwt",
     },
-    secret:process.env.NEXT_AUTH_SECRET,*/
-}
+    secret: process.env.NEXT_AUTH_SECRET,
+};
