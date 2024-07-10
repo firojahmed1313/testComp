09e9978a6aeceb0b01d91e7d6e6c7388f9921a1a
MD FIROJ AHMED <firozahmed3052000@gmail.com>
2024-07-10 12:37:12 +0000

    olaapi

---
+++ OLAMAP/olamap/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import react, { useEffect, useState } from 'react';
+//require('dotenv').config()
+function App() {
+  //console.log(projectId);
+  return (
+    <div className="App">
+      <h2>MD FIROJ AHMED</h2>
+      <h2>MD FIROJ AHMED</h2>
+      <h2>MD FIROJ AHMED</h2>
+      <h2>MD FIROJ AHMED</h2>
+    </div>
+  );
+}
+
+export default App;
