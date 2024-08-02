a3f462a6d59b6bcb5dc03309e99b00b58619a1dd
MD FIROJ AHMED <firozahmed3052000@gmail.com>
2024-08-02 14:27:32 +0000

    app view urls include and template

--- Django/Chai_tutoral/chaiaurdjango/chaiaurdjango/urls.py
+++ Django/Chai_tutoral/chaiaurdjango/chaiaurdjango/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from . import views
+from django.urls import path,include
+from . import views 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomePage, name='home'),
     path('about/', views.AboutPage, name='about'),
     path('contact/', views.ContactPage, name='contact'),
-    path('blog/', views.BlogPage, name='blog'),  # Added new URL pattern for BlogPage.
+    path('blog/', views.BlogPage, name='blog'), 
+    path('test',include('appTest.urls')),
 ]
