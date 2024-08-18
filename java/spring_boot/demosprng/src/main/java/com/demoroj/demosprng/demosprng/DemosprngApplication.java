ec16406aaa2673640acf7f1aa9b2aea7b642c8c1
MD FIROJ AHMED <firozahmed3052000@gmail.com>
2024-08-18 10:04:25 +0000

    spring_boot setup vscode

---
+++ java/spring_boot/demosprng/src/main/java/com/demoroj/demosprng/demosprng/DemosprngApplication.java
@@ -0,0 +1,13 @@
+package com.demoroj.demosprng.demosprng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosprngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosprngApplication.class, args);
+	}
+
+}
---
+++ java/spring_boot/demosprng/src/test/java/com/demoroj/demosprng/demosprng/DemosprngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoroj.demosprng.demosprng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosprngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
